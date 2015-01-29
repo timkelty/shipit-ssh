69323c8b75cb5632fd2569a0c727952fa002ca1b
Tim Kelty <tim@fusionary.com>
2015-01-29 01:11:21 -0500

    Init

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = function (shipit) {
+  require('./tasks/ssh')(shipit);
+};
---
+++ tasks/ssh/index.js
@@ -0,0 +1,11 @@
+/* jshint unused:false */
+var registerTask = require('../../lib/register-task');
+var getShipit = require('../../lib/get-shipit');
+
+module.exports = function (gruntOrShipit) {
+  registerTask(gruntOrShipit, 'ssh', task);
+
+  function task() {
+    var shipit = getShipit(gruntOrShipit);
+  }
+};
