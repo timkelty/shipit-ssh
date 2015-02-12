5d1ad5b1cbc4146bcbe7f7a621e953873ff5c059
Tim Kelty <tim@fusionary.com>
2015-02-12 11:01:51 -0500

    Cleanup

--- tasks/ssh/index.js
+++ tasks/ssh/index.js
@@ -1,12 +1,13 @@
-/* jshint unused:false */
 var registerTask = require('../../lib/register-task');
 var getShipit = require('../../lib/get-shipit');
 
 module.exports = function (gruntOrShipit) {
-  registerTask(gruntOrShipit, 'ssh', task);
-
-
-  function task() {
+  var task = function task() {
     var shipit = getShipit(gruntOrShipit);
-  }
+    var server = Array.isArray(shipit.config.servers) ? shipit.config.servers[0] : shipit.config.servers;
+    var cmd = ['ssh', '-T', server];
+    return shipit.local(cmd.join(' '));
+  };
+
+  registerTask(gruntOrShipit, 'ssh', task);
 };
