c65dc2982182656053329f79e605261ec13855e6
Tim Kelty <tim@fusionary.com>
2015-02-12 11:19:18 -0500

    SSH command

--- tasks/ssh/index.js
+++ tasks/ssh/index.js
@@ -5,7 +5,12 @@ module.exports = function (gruntOrShipit) {
   var task = function task() {
     var shipit = getShipit(gruntOrShipit);
     var server = Array.isArray(shipit.config.servers) ? shipit.config.servers[0] : shipit.config.servers;
-    var cmd = ['ssh', '-T', server];
+    var cmd = [
+      'ssh',
+      server,
+      '-t',
+        ['\'', 'cd', shipit.config.deployTo, ';bash --login', '\''].join(' ')
+    ];
     return shipit.local(cmd.join(' '));
   };
 
