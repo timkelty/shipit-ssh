4fd330c63caa8b764ba2db52facfc8f2a0fb56f6
Tim Kelty <tim@fusionary.com>
2015-02-20 14:20:52 -0500

    Cleanup

--- tasks/ssh/index.js
+++ tasks/ssh/index.js
@@ -1,11 +1,10 @@
-var registerTask = require('../../lib/register-task');
-var getShipit = require('../../lib/get-shipit');
+var utils = require('shipit-utils');
 var remoteInteractive = require('../../lib/remote-interactive');
 var inquirer = require('bluebird-inquirer');
 
 module.exports = function (gruntOrShipit) {
   var task = function task() {
-    var shipit = getShipit(gruntOrShipit);
+    var shipit = utils.getShipit(gruntOrShipit);
     shipit = remoteInteractive(shipit);
     var remoteCmd = ['cd', shipit.config.deployTo + ';', 'bash --login'].join(' ');
     var servers = Array.isArray(shipit.config.servers) ? shipit.config.servers : [shipit.config.servers];
@@ -29,5 +28,5 @@ module.exports = function (gruntOrShipit) {
 
   };
 
-  registerTask(gruntOrShipit, 'ssh', task);
+  utils.registerTask(gruntOrShipit, 'ssh', task);
 };
