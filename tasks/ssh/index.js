575a671b12e6fdb05c1a4b54e2ab06f7132a85e9
Tim Kelty <tim@fusionary.com>
2015-02-20 12:59:35 -0500

    Only check if multiple servers

--- tasks/ssh/index.js
+++ tasks/ssh/index.js
@@ -8,9 +8,10 @@ module.exports = function (gruntOrShipit) {
     var shipit = getShipit(gruntOrShipit);
     shipit = remoteInteractive(shipit);
     var remoteCmd = ['cd', shipit.config.deployTo + ';', 'bash --login'].join(' ');
+    var servers = Array.isArray(shipit.config.servers) ? shipit.config.servers : [shipit.config.servers];
 
-    // Ask for server if array
-    if (Array.isArray(shipit.config.servers)) {
+    // Ask for server if multiple
+    if (servers.length > 1) {
 
       return inquirer.prompt([{
         type: 'list',
@@ -23,7 +24,7 @@ module.exports = function (gruntOrShipit) {
       });
 
     } else {
-      return shipit.remoteInteractive(shipit.config.servers, remoteCmd);
+      return shipit.remoteInteractive(shipit.config.servers[0], remoteCmd);
     }
 
   };
